cb6df2eb8472275edec7532e7da7ae5837ae77a3
dylansarikas <dylan.sarikas@gmail.com>
2024-06-14 15:54:26 -0500

    adds photo index html

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
-    @all_photos = Photo.all
+    @all_photos = Photo.all.order({ :created_at => :desc })
 
     render({ :template=>"photo_templates/index" })
   end
