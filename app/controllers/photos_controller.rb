5999620361ab1d335fb2ef4244bbb6ac7032f099
dylansarikas <dylan.sarikas@gmail.com>
2024-06-14 16:20:36 -0500

    adds photo show route

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,12 @@ class PhotosController < ApplicationController
   def index
     @all_photos = Photo.all.order({ :created_at => :desc })
 
-    render({ :template=>"photo_templates/index" })
+    render({ :template => "photo_templates/index" })
+  end
+
+  def show
+    @this_photo = Photo.where({ :id => params[:id] })
+
+    render({ :template => "photo_templates/show" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'photos/index'
   get "/users" => "users#index"
   get "/users/:username" => "users#show"
 
   get "/photos" => "photos#index"
+  get "/photos/:id" => "photos#show"
 end
