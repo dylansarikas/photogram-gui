86c0c5e6a5a46945d8d97d8523deff818f4b67ba
dylansarikas <dylan.sarikas@gmail.com>
2024-06-14 14:54:27 -0500

    adds uses index html page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
+    @all_users = User.all.order({ :username => :asc })
+
     render({ :template => "user_templates/index" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
   get "/users" => "users#index"
+  get "/users/:username" => ""
 
 end
